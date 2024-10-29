54367ae98a06704b5b8fbb87375a88406b9b84c1
AntonioCarri11o <20203tn078@utez.edu.mx>
2024-10-28 23:56:40 -0600

    Details restaurant

--- lib/widgets/modules/restaurants/screens/restaurant_details.dart
+++ lib/widgets/modules/restaurants/screens/restaurant_details.dart
@@ -1,34 +1,103 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_rating/flutter_rating.dart';
 import "package:learning_flutter/widgets/modules/restaurants/entities/restaurant.dart";
-class RestaurantDetails extends StatefulWidget {
-const RestaurantDetails({super.key, required this.id_restaurant,});
 
-  final String id_restaurant;
+class RestaurantDetails extends StatefulWidget {
+  const RestaurantDetails({
+    super.key,
+    required this.restaurant,
+  });
+
+  final Restaurant restaurant;
   @override
   State<RestaurantDetails> createState() => _RestaurantDetailsState();
 }
 
 class _RestaurantDetailsState extends State<RestaurantDetails> {
-  final FirebaseFirestore db = FirebaseFirestore.instance;
   bool _isLoading = true;
-  //final Restaurant restaurant;
+  String title = 'Detalles';
   @override
-  void initState() {
-    super.initState();
-
-  }
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalles'),
+        title: Text(title),
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('restaurants')
+            .doc(widget.restaurant.id)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (!snapshot.hasData ||
+              snapshot.data == null ||
+              !snapshot.data!.exists) {
+            return const Text("No se encontró eñ restaurante");
+          }
+          Restaurant restaurantSnapshot = Restaurant(
+              widget.restaurant.id,
+              snapshot.data!.get('name'),
+              snapshot.data!.get('description'),
+              List<String>.from(snapshot.data!.get('images')),
+              snapshot.data!.get('rating'),
+              snapshot.data!.get('count'));
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 300.0,
+                  aspectRatio: 16 / 9,
+                ),
+                items: restaurantSnapshot.images.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Image.network(i));
+                    },
+                  );
+                }).toList(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          restaurantSnapshot.name,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24),
+                        ),
+                        const Spacer(),
+                        StarRating(
+                          rating: restaurantSnapshot.rating,
+                          color: Colors.orange,
+                          borderColor: Colors.grey,
+                          starCount: 5,
+                          size: 18,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(restaurantSnapshot.description)
+                  ],
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
